7092412a71a1a1811c1ab842b5ddecf65f45f37a
Petro Fedorenko <petewaitforletters@gmail.com>
2020-08-13 18:13:33 +0300

    feat: implement Answers component

---
+++ src/components/Answers/Answers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Answers.scss';
+
+function Answers() {
+  return (
+    <div className="answers">
+      <button className="answers__button">1</button>
+      <button className="answers__button">2</button>
+      <button className="answers__button">3</button>
+      <button className="answers__button">4</button>
+      <button className="answers__button">5</button>
+      <button className="answers__button">6</button>
+    </div>
+  );
+}
+
+export default Answers;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.scss';
 import Header from '../Header';
 import Question from '../Question';
+import Answers from '../Answers';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Question />
+      <div className="App__answers-and-info">
+        <Answers />
+      </div>
+      
     </div>
   );
 }
